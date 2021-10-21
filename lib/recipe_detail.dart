92a11829d78a4ff74ff8f1a9380a4a6a4af8b20f
Jamie Morgan <jmorgan3475@gmail.com>
2021-10-21 11:19:51 -0600

    Add ingredients to details

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -20,22 +20,34 @@ class _RecipeDetailState extends State<RecipeDetail> {
         title: Text(widget.recipe.label),
       ),
       body: SafeArea(
-          child: Column(
-        children: [
-          SizedBox(
-            height: 300,
-            width: double.infinity,
-            child: Image(
-              image: AssetImage(widget.recipe.imageUrl),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image(
+                image: AssetImage(widget.recipe.imageUrl),
+              ),
             ),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            widget.recipe.label,
-            style: const TextStyle(fontSize: 18),
-          )
-        ],
-      )),
+            const SizedBox(height: 4),
+            Text(
+              widget.recipe.label,
+              style: const TextStyle(fontSize: 18),
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  return Text(
+                      '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
